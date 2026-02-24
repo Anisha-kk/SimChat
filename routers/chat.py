18b933003beb8f0a782a81d0904856d1587f56c9
Anisha-kk <akk30272419@gmail.com>
2026-02-24 17:05:09 +0530

    Modified partial match code

--- routers/chat.py
+++ routers/chat.py
@@ -30,13 +30,20 @@ def Chat(input:Message,rules=Depends(get_rules)):
         return {"response": rules[query]}
 
     # Partial match
+    sim_query = None #To store the query in the rule base that has the highest match with the input query.
+    max_prob=0
     for queries, responses in rules.items():
         similarity = difflib.SequenceMatcher(None, query, queries).ratio() 
-        if similarity > 0.8:#If similarity between strings are more than 80%, return the response
-            result = dynamic_query_result(queries)#Returns an outcome if the query response is dynamic
-            if result:
-                return result
-            return {"response": responses}
+        if similarity > 0.85:#If similarity between strings are more than 85%, return the response
+            if max_prob<similarity:
+                max_prob = similarity
+                sim_query = queries
+    if sim_query:#There is a partially matching query in the rule base
+        result = dynamic_query_result(sim_query)#Returns an outcome if the query response is dynamic
+        if result:
+            return result
+        else:
+            return {"response": rules[sim_query]}
 
     # Failure response
     return {"response": "Sorry! I do not have an answer to this question!"}
